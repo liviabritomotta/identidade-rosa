ac6dd2260d7a8fa79ebe106cef1cf252bf4c071a
liviabritomotta <livia.motta@sptech.school>
2026-05-04 13:16:14 -0300

    corrige web-data-viz no projeto

---
+++ src/controllers/quizController.js
@@ -0,0 +1,30 @@
+var quizModel = require("../models/quizModel");
+
+function cadastrar(req, res) {
+    var fkUsuario = req.body.fkUsuarioServer;
+    var moda = req.body.modaServer;
+    var tech = req.body.techServer;
+    var criativa = req.body.criativaServer;
+    var esporte = req.body.esporteServer;
+    var estudo = req.body.estudoServer;
+    var maquiar = req.body.maquiarServer;
+    var viagem = req.body.viagemServer;
+    var resultado = req.body.resultadoServer;
+
+    if (fkUsuario == undefined) {
+        res.status(400).send("fkUsuario está undefined!");
+    } else {
+        quizModel.cadastrar(fkUsuario, moda, tech, criativa, esporte, estudo, maquiar, viagem, resultado)
+            .then(function (resposta) {
+                res.json(resposta);
+            })
+            .catch(function (erro) {
+                console.log(erro);
+                res.status(500).json(erro.sqlMessage);
+            });
+    }
+}
+
+module.exports = {
+    cadastrar
+};
