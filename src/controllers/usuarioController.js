e2cc2da01bf77ae14df9be93711cf51d37392ca4
liviabritomotta <livia.motta@sptech.school>
2026-05-18 12:59:53 -0300

    começando a adicionar rotas para KPIs e novo grafico da dash

--- src/controllers/usuarioController.js
+++ src/controllers/usuarioController.js
@@ -47,6 +47,7 @@ function cadastrar(req, res) {
     // Crie uma variável que vá recuperar os valores do arquivo cadastro.html
     var nome = req.body.nomeServer;
     var email = req.body.emailServer;
+    var nascimento =req.body.nascimentoServer;
     var senha = req.body.senhaServer;
     
 
@@ -55,12 +56,14 @@ function cadastrar(req, res) {
         res.status(400).send("Seu nome está undefined!");
     } else if (email == undefined) {
         res.status(400).send("Seu email está undefined!");
+         } else if (nascimento == undefined) {
+        res.status(400).send("Sua data de nascimento está undefined!");
     } else if (senha == undefined) {
         res.status(400).send("Sua senha está undefined!");
     }  else {
 
         // Passe os valores como parâmetro e vá para o arquivo usuarioModel.js
-        usuarioModel.cadastrar(nome, email, senha)
+        usuarioModel.cadastrar(nome, email, nascimento, senha)
             .then(
                 function (resultado) {
                     res.json(resultado);
--- src/database/script-tabelas.sql
+++ src/database/script-tabelas.sql
@@ -15,6 +15,7 @@ CREATE TABLE usuario (
 	id INT PRIMARY KEY AUTO_INCREMENT,
 	nome VARCHAR(50),
 	email VARCHAR(50),
+    datadenascimento date,
 	senha VARCHAR(50)
 );
 
